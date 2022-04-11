4eb6093cd95fcbbd4894a5f2b55c79a15c2f24fe
IvanMonroyPOLI <ivmonroy4@poligran.edu.co>
2022-04-11 00:35:52 -0500

    creación de api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
+  def authenticate_user!
+    return true
+  end
+  def status
+    render json: { data: 'ok' }
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,21 @@ module ApiPoliIvanmonroy
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource(
+          '*',
+          headers: :any,
+          methods: [:get, :patch, :put, :delete, :post, :options]
+        )
+      end
+    end
+
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => '*'
+    }
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
